1e4366e946ee5c292ac2afc497b0b59b18c9eeb3
Anirudh Sylendranath <ani.for.prez@gmail.com>
2018-01-29 19:14:50 +0530

    Init Vue project made using vue-cli

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    // Override default space bullshit for tabs cause tabs are the best and fuck you
+    "indent": ["error", "tab"],
+    // No space after conditionals or loops because WHY
+    "keyword-spacing": ["error", { "overrides": { "if": { "after": false }, "for": { "after": false }, "while": { "after": false } } }],
+    // No space before function parenthesis because WHY
+    "space-before-function-paren": ["error", "never"],
+    // Semicolons BECAUSE WHY THE FUCK NOT
+    "semi": ["error", "always"],
+    // Override of default no tabs bullshit
+    "no-tabs": 0,
+
+    // allow async-await
+    'generator-star-spacing': ["error", {"before": true, "after": false}],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
